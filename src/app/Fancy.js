f47c90449a2ab315df8ac9c2a2ab4be67e5bb3f5
kuznetsov_e <jackk@yandex.ru>
2017-04-23 21:33:47 +0300

    Some files added.

---
+++ src/app/Fancy.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions
+} from 'react-native';
+import {styles} from './styles';
+
+export class Fancy extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <View style={styles.box}>
+          
+          </View>
+          <Text style={styles.text}>
+            Fancy
+          </Text>
+        </View>
+      )
+    }
+}
+
