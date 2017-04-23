54103273fa9ed8243c02aa51dede6e686ef5a88d
kuznetsov_e <jackk@yandex.ru>
2017-04-23 13:31:13 +0300

    Todo skeleton is over.

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -7,10 +7,40 @@ import {
 } from 'react-native';
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ''
+    }
+  }
+  
+  handleChange(text) {
+    this.setState({newTodo: text})
+  }
+  
+  handlePress() {
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({todos, newTodo: ""})
+  }
   
   render() {
     return (
-      <Text>hello</Text>
+      <View>
+        <TextInput
+          value={this.state.newTodo}
+          onChangeText={this.handleChange.bind(this)}
+          />
+        
+        <TouchableOpacity
+          onPress={this.handlePress.bind(this)}>
+            <Text>Add</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.todos.map((todo, i) => 
+            <Text key={i}>{todo}</Text>)}
+        </View>
+      </View>
     )
   }
 }
