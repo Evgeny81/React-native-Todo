ebf835c1111f42d0232f8edda3e3d312b6ca5975
kuznetsov_e <jackk@yandex.ru>
2017-04-23 18:42:15 +0300

    Style is added.

--- index.ios.js
+++ index.ios.js
@@ -8,7 +8,8 @@ import React, { Component } from 'react';
 import {
   AppRegistry
 } from 'react-native';
-import { Todo } from './src/app/Todo'
+import { Todo } from './src/app/Todo';
+import { Fancy } from './src/app/Fancy';
 
 const Main = () => (<Todo />)
 
--- src/app/Todo.js
+++ src/app/Todo.js
@@ -3,7 +3,9 @@ import {
   Text,
   View,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  StyleSheet,
+  Switch
 } from 'react-native';
 
 export class Todo extends Component {
@@ -21,26 +23,77 @@ export class Todo extends Component {
   
   handlePress() {
     const todos = [...this.state.todos, this.state.newTodo];
+    console.warn(JSON.stringify(this.state, null, 2));
     this.setState({todos, newTodo: ""})
   }
   
   render() {
     return (
-      <View>
-        <TextInput
-          value={this.state.newTodo}
-          onChangeText={this.handleChange.bind(this)}
-          />
+      <View style = {styles.container}>
+        <Switch />
+        <View style={styles.form}>
+          <TextInput
+            style = {styles.input}
+            value={this.state.newTodo}
+            onChangeText={this.handleChange.bind(this)}
+            />
+          <TouchableOpacity
+            style = {styles.button}
+            onPress={this.handlePress.bind(this)}>
+              <Text style={styles.buttonText}>Add</Text>
+          </TouchableOpacity>
+        </View>
         
-        <TouchableOpacity
-          onPress={this.handlePress.bind(this)}>
-            <Text>Add</Text>
-        </TouchableOpacity>
-        <View>
+        <View style = {styles.todos}>
           {this.state.todos.map((todo, i) => 
-            <Text key={i}>{todo}</Text>)}
+            <View style = {styles.todo} key={i}>
+              <Text style = {styles.todoText}>{todo}</Text>
+            </View>
+            )}
         </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10
+  },
+  form: {
+//     flex: 1,
+    flexDirection: 'row'
+  },
+  input: {
+    flex: 0.7,
+    fontSize: 24,
+    
+  },
+  button: {
+    flex: 0.3,
+    height: 50,
+    borderWidth: 1,
+    borderColor: 'lightblue',
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  todos: {
+    marginTop: 60,
+    
+  },
+  todo: {
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey'
+  },
+  todoText: {
+    fontSize: 24
+  }
+})
+
